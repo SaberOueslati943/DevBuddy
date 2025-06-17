73bd0f8a8067db210636e87fbea1030c1fafc299
Saber Oueslati <saber.oueslati943@gmail.com>
2025-06-17 23:15:20 +0100

    Modify features composables
    - Set a background for Feature1 and Feature2.

--- app/src/main/java/com/saberoueslati/devbuddy/features/feature1/Feature1.kt
+++ app/src/main/java/com/saberoueslati/devbuddy/features/feature1/Feature1.kt
@@ -1,18 +1,24 @@
 package com.saberoueslati.devbuddy.features.feature1
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import kotlin.random.Random
 
 @Composable
 fun Feature1(
     onButtonClick: (Int) -> Unit
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+    ) {
         Text("Feature1")
         Button(onClick = {
             onButtonClick(Random.nextInt())
--- app/src/main/java/com/saberoueslati/devbuddy/features/feature2/Feature2.kt
+++ app/src/main/java/com/saberoueslati/devbuddy/features/feature2/Feature2.kt
@@ -1,16 +1,22 @@
 package com.saberoueslati.devbuddy.features.feature2
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Feature2(
     id: Int
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
         Text("Feature2 with Id: $id")
     }
 }
